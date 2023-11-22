ac01b81da69ae2c621400c2a69c1819f2dba95f9
LieneV-D <vecvagare.liene@gmail.com>
2023-11-22 17:01:28 +0200

    Added week day and current time with JS
    h2 displays current day of the week and current time

---
+++ src/app.js
@@ -0,0 +1,27 @@
+let currentDate = document.querySelector("#current-time");
+let currentTime = new Date();
+let minutes = currentTime.getMinutes();
+let hours = currentTime.getHours();
+let day = currentTime.getDay();
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let formattedDay = days[day];
+
+currentDate.innerHTML = `${formattedDay}, ${hours}:${minutes}`;
